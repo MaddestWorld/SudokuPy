94caa676b6b88bf073c329d7c07668b6d1f9ea6b
MaddestWorld <luka.markelj97@gmail.com>
2018-03-20 12:02:03 +0100

    #se nekaj popravkov, da bo generiranje delovalo optimalno

--- sudoku.py
+++ sudoku.py
@@ -3,48 +3,66 @@ from random import randint
 class Sudoku():
     ''''''
     def __init__(self):
-        self.sodoku=list(""*9)
+        self.sudoku=[[0]*9]*9
         self.nedSodoku=list()
 
     def generiraj(self):
         '''Generira Sudoku pri tem pazi na pravilnost sudokuja %%nedokoncano%%'''
-        strst=0
-        self.sodoku=list("1"*9)
-        print(self.sodoku)
-        for i in range(len(self.sodoku)):
-            self.sodoku[i]=""
-            while len(self.sodoku[i])<=9:
-                print(self.sodoku)
-                strst=str(randint(1,9))
-                if strst in self.sodoku[i]:
-                    pass
-                else:
-                   if len(self.sodoku[i])<=3:
-                       if i<3:
-                           if strst in self.sodoku[i-1]:
-                               pass
-                           elif i==2 and (strst in self.sodoku[i-2]):
-                               pass
-                           else:
-                               self.sodoku[i]+=strst
-                       elif i<6:
-                           if i==4 and (strst in self.sodoku[i-1]):
-                               pass
-                           elif i==5 and (strst in self.sodoku[i-2]):
-                               pass
-                           else:
-                               self.sudoku[i]+=strst
-                       else:
-                           if i==7 and (strst in self.sodoku[i-1]):
-                               pass
-                           elif i==8 and (strst in self.sodoku[i-2]):
-                               pass
-                           else:
-                               self.sodoku[i]+=strst
+        self.sudoku = [[0] * 9] * 9
+        seznamStolpcev=[[0] * 9] * 9
+        seznamTrojk=[[0]*9]*9
+        stPonovitev=0
+        for vrstica in range(9):
+            for stolpec in range(9):
+                while "":
+                    stevilo=randint(1,9)
+                    if (stevilo not in self.sudoku[vrstica]) and (stevilo not in seznamStolpcev[vrstica]):
+                        if stolpec<=2 and vrstica<=2:
+                            if stevilo not in seznamTrojk[0]:
+                                self.sudoku[vrstica][stolpec]=stevilo
+                        elif stolpec<=5 and vrstica<=2:
+                            if stevilo not in seznamTrojk[1]:
+                                self.sudoku[vrstica][stolpec]=stevilo
+                        elif stolpec<=8 and vrstica<=2:
+                            if stevilo not in seznamTrojk[2]:
+                                self.sudoku[vrstica][stolpec]=stevilo
+                        elif stolpec<=2 and vrstica<=5:
+                            if stevilo not in seznamTrojk[3]:
+                                self.sudoku[vrstica][stolpec]=stevilo
+                        elif stolpec<=5 and vrstica<=5:
+                            if stevilo not in seznamTrojk[4]:
+                                self.sudoku[vrstica][stolpec]=stevilo
+                        elif stolpec<=8 and vrstica<=5:
+                            if stevilo not in seznamTrojk[5]:
+                                self.sudoku[vrstica][stolpec]=stevilo
+                        elif stolpec<=2 and vrstica<=8:
+                            if stevilo not in seznamTrojk[6]:
+                                self.sudoku[vrstica][stolpec]=stevilo
+                        elif stolpec<=5 and vrstica<=8:
+                            if stevilo not in seznamTrojk[7]:
+                                self.sudoku[vrstica][stolpec]=stevilo
+                        elif stolpec<=8 and vrstica<=8:
+                            if stevilo not in seznamTrojk[8]:
+                                self.sudoku[vrstica][stolpec]=stevilo
+
+                    #posodobitev tabele stolpcev
+                    if "":
+                    seznamStolpcev[stolpec][vrstica]=self.sudoku[vrstica][stolpec]
+                    if "":
+                    #posodobitev tablele
+                    seznamTrojk[stolpec%3]=self.sudoku[vrstica][stolpec]
+
+
+
+
+
+
+
+
 
     def pobrisiElemente(self,tezavnost):
         '''Pripravi sodoku za resevanje, to naradi tako da uposteva tezavnost ter na tezavnost izbrise st podatkov. '''
-        self.nedSodoku=self.sodoku
+        self.nedSodoku=self.sudoku
         stElementovZaPobrisati=81-tezavnost
         while stElementovZaPobrisati>0:
             vrstica=randint(0,8)
@@ -55,4 +73,4 @@ class Sudoku():
 
 novo1=Sudoku()
 novo1.generiraj()
-print(novo1.sodoku)
+print(novo1.sudoku)
